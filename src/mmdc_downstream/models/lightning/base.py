bb6f38bf290961cf7a0eb304843233af13fd5f94
Ekaterina <ek.kalinicheva@gmail.com>
2023-11-21 14:47:11 +0100

    Lightning modules

---
+++ src/mmdc_downstream/models/lightning/base.py
@@ -0,0 +1,177 @@
+# Copyright: (c) 2022 CESBIO / Centre National d'Etudes Spatiales
+""" Base classes for the MMDC Lightning module and Pytorch model"""
+import logging
+from abc import abstractmethod
+from typing import Any
+
+import torch
+from mmdc_singledate.datamodules.datatypes import ShiftScale, MMDCDataStats, MMDCShiftScales
+from pytorch_lightning import LightningModule
+from torchmetrics import MinMetric
+from torchutils import metrics
+
+from mmdc_downstream.mmdc_model.model import PretrainedMMDC
+from ..torch.base import MMDCDownstreamBaseModule
+
+# Configure logging
+NUMERIC_LEVEL = getattr(logging, "INFO", None)
+logging.basicConfig(level=NUMERIC_LEVEL,
+                    format="%(asctime)-15s %(levelname)s: %(message)s")
+
+logger = logging.getLogger(__name__)
+
+
+class MMDCDownstreamBaseLitModule(LightningModule):  # pylint: disable=too-many-ancestors
+    """
+    Base Lightning Module for the MMDC Single Date networks
+    """
+
+    def __init__(self,
+                 model: MMDCDownstreamBaseModule,
+                 model_mmdc: PretrainedMMDC | None = None,
+                 lr: float = 0.001,
+                 resume_from_checkpoint: str | None = None,
+                 stats_path: str = None):
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        # it also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False)
+        self.model = model
+        self.resume_from_checkpoint = resume_from_checkpoint
+
+        # use separate metric instance for train, val and test step
+        # to ensure a proper reduction over the epoch
+        self.test_rmse = metrics.rmse
+
+        # for logging best so far validation rmse
+        self.val_rmse_best = MinMetric()
+        self.learning_rate = lr
+
+        self.model_mmdc: PretrainedMMDC = model_mmdc
+
+        if stats_path is not None:
+            self.stats = self.get_stats(torch.load(stats_path))
+
+    def get_stats(self, stats: MMDCDataStats) -> MMDCShiftScales:
+        """Set shift and scale for model"""
+        scale_regul = torch.nn.Threshold(1e-10, 1.0)
+        shift_scale_s2 = ShiftScale(
+            stats.sen2.median,
+            scale_regul((stats.sen2.qmax - stats.sen2.qmin) / 2.0),
+        )
+        shift_scale_s1 = ShiftScale(
+            stats.sen1.median,
+            scale_regul((stats.sen1.qmax - stats.sen1.qmin) / 2.0),
+        )
+        shift_scale_meteo = ShiftScale(
+            stats.meteo.concat_stats().median,
+            scale_regul((stats.meteo.concat_stats().qmax -
+                         stats.meteo.concat_stats().qmin) / 2.0),
+        )
+        shift_scale_dem = ShiftScale(
+            stats.dem.median,
+            scale_regul((stats.dem.qmax - stats.dem.qmin) / 2.0),
+        )
+
+        return MMDCShiftScales(
+            shift_scale_s2,
+            shift_scale_s1,
+            shift_scale_meteo,
+            shift_scale_dem,
+        )
+
+    @abstractmethod
+    def step(self, batch: Any) -> Any:
+        """Perform one optimization step"""
+
+    @abstractmethod
+    def forward(self, data: Any) -> Any:  # pylint: disable=arguments-differ
+        """Generic forward pass of the model. Just delegate to the Pytorch model."""
+
+    def training_step(  # pylint: disable=arguments-differ
+            self,
+            batch: Any,
+            batch_idx: int,  # pylint: disable=unused-argument
+    ) -> dict[str, Any]:
+        """Training step. Step and return loss."""
+        torch.autograd.set_detect_anomaly(True)
+        loss = self.step(batch)
+
+        # log training metrics
+        self.log(
+            "train/loss",
+            loss,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=False,
+        )
+
+        return {"loss": loss}
+
+    def validation_step(  # pylint: disable=arguments-differ
+            self,
+            batch: Any,
+            batch_idx: int,  # pylint: disable=unused-argument
+    ) -> dict[str, Any]:
+        """Validation step. Step and return loss."""
+        loss = self.step(batch)
+
+        # log val metrics
+        self.log(
+            "val/loss",
+            loss,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=False,
+        )
+        return {"loss": loss}
+
+    def test_step(  # pylint: disable=arguments-differ
+            self,
+            batch: Any,
+            batch_idx: int,  # pylint: disable=unused-argument
+    ) -> dict[str, Any]:
+        """Test step. Step and return loss."""
+        loss = self.step(batch)
+
+        self.log(
+            "test/loss",
+            loss,
+        )
+
+        return {"loss": loss}
+
+    def on_train_epoch_end(self) -> None:
+        logger.info("Ended traning epoch %s", self.trainer.current_epoch)
+
+    def on_validation_epoch_end(self) -> None:
+        logger.info("Ended validation epoch %s", self.trainer.current_epoch)
+
+    def on_test_epoch_end(self) -> None:
+        """Callback after a test epoch"""
+
+    def on_fit_start(self) -> None:
+        """
+        On fit start, load pretrained MMDC model if its path is set.
+        The scales are already set in the loaded model
+        """
+        logger.info("On fit start")
+
+    def configure_optimizers(self) -> dict[str, Any]:
+        """A single optimizer with a LR scheduler"""
+        optimizer = torch.optim.Adam(params=self.model.parameters(),
+                                     lr=self.learning_rate)
+
+        training_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
+            optimizer, T_0=4, T_mult=2, eta_min=0, last_epoch=-1)
+        # scheduler = {
+        #     "scheduler": training_scheduler,
+        #     "interval": "epoch",
+        #     "monitor": "val/loss",
+        #     "frequency": 1,
+        # }
+        return {
+            "optimizer": optimizer,
+            # "lr_scheduler": scheduler,
+        }
---
+++ src/mmdc_downstream/models/lightning/lai_regression.py
@@ -0,0 +1,168 @@
+from typing import Any
+
+import torch
+from mmdc_singledate.datamodules.datatypes import MMDCBatch
+from mmdc_singledate.datamodules.mmdc_datamodule import destructure_batch
+from mmdc_singledate.utils.train_utils import standardize_data
+
+from mmdc_downstream.mmdc_model.model import PretrainedMMDC
+from .base import MMDCDownstreamBaseLitModule
+from ..components.losses import mmdc_mse
+from ..datatypes import OutputLAI
+from ..torch.lai_regression import MMDCDownstreamRegressionModule
+from ...snap.components.compute_bio_var import predict_variable_from_tensors, stand_lai, unstand_lai, prepare_s2_image
+from ...snap.lai_snap import BVNET
+
+
+class MMDCDownstreamRegressionLitModule(MMDCDownstreamBaseLitModule):
+    """
+    LAI regression lightning module.
+    Attributes:
+        model: regression model
+        model_snap: Fixed weights SNAP model for LAI GT computation
+        model_mmdc: Pretrained MMDC model for latent representations
+    """
+
+    def __init__(
+            self,
+            model: MMDCDownstreamRegressionModule,
+            model_snap: BVNET,
+            model_mmdc: PretrainedMMDC | None = None,
+            input_data: str = "experts",
+            lr: float = 0.001,
+            resume_from_checkpoint: str | None = None,
+    ):
+        super().__init__(model, model_mmdc, lr, resume_from_checkpoint)
+
+        self.model_snap = model_snap
+        self.model_snap.set_snap_weights()
+        self.input_data = input_data
+
+    def get_regression_input(self, batch: MMDCBatch) -> torch.Tensor:
+        """
+        Prepare input for downstream model.
+        It can be normalized S1/S2 data or one of latent representations
+        produced by MMDC model.
+        """
+        # [experts, lat_S1, lat_S2, S2, S1_asc, S1_desc]
+        if self.input_data == "experts":
+            return self.model_mmdc.get_latent_mmdc(batch).latent_experts_mu
+        elif self.input_data == "lat_S1":
+            return self.model_mmdc.get_latent_mmdc(batch).latent_S1_mu
+        elif self.input_data == "lat_S2":
+            return self.model_mmdc.get_latent_mmdc(batch).latent_S2_mu
+        elif self.input_data == "S2":
+            s2_x = standardize_data(
+                batch.s2_x,
+                shift=self.stats.sen2.shift.type_as(
+                    batch.s2_x),
+                scale=self.stats.sen2.shift.type_as(
+                    batch.s2_x),
+            )
+            return prepare_s2_image(s2_x, batch.s2_a, reshape=False)
+        else:
+            s1_x = standardize_data(
+                batch.s1_x,
+                shift=self.stats.sen1.shift.type_as(
+                    batch.s1_x),
+                scale=self.self.stats.sen1.shift.type_as(
+                    batch.s1_x),
+            )
+            if self.input_data == "S1_asc":
+                return s1_x[:, :3]
+            else:  # "S1_desc":
+                return s1_x[:, 3:]
+
+    def step(self, batch: Any) -> Any:
+        """One step"""
+        batch: MMDCBatch = destructure_batch(batch)
+        margin = self.model_mmdc.model_mmdc.nb_cropped_hw
+        lai_gt = self.compute_gt(batch)
+        reg_input = self.get_regression_input(batch)
+        lai_pred = self.forward(reg_input)
+        loss_mse = mmdc_mse(lai_pred[:, :, margin:-margin, margin:-margin],
+                            lai_gt[:, :, margin:-margin, margin:-margin],
+                            batch.s2_m[:, :, margin:-margin, margin:-margin])
+        return loss_mse
+
+    def training_step(  # pylint: disable=arguments-differ
+            self,
+            batch: Any,
+            batch_idx: int,  # pylint: disable=unused-argument
+    ) -> dict[str, Any]:
+        """Training step. Step and return loss."""
+        torch.autograd.set_detect_anomaly(True)
+        reg_loss = self.step(batch)
+        self.log(
+            "train/loss",
+            reg_loss,
+            on_step=True,
+            on_epoch=True,
+            prog_bar=False,
+        )
+
+        return {"loss": reg_loss}
+
+    def validation_step(  # pylint: disable=arguments-differ
+            self,
+            batch: Any,
+            batch_idx: int,  # pylint: disable=unused-argument
+            prefix: str = "val",
+    ) -> dict[str, Any]:
+        """Validation step. Step and return loss."""
+        reg_loss = self.step(batch)
+
+        self.log(
+            f"{prefix}/loss",
+            reg_loss,
+            on_step=True,
+            on_epoch=True,
+            prog_bar=False,
+        )
+
+        return {"loss": reg_loss}
+
+    def test_step(  # pylint: disable=arguments-differ
+            self,
+            batch: Any,
+            batch_idx: int,  # pylint: disable=unused-argument
+    ) -> dict[str, Any]:
+        """Test step. Step and return loss. Delegate to validation step"""
+        return self.validation_step(batch, batch_idx, prefix="test")
+
+    def forward(self, data: torch.Tensor) -> torch.Tensor:
+        """Forward step"""
+        return self.model.forward(data)
+
+    def predict(self, batch: MMDCBatch) -> OutputLAI:
+        self.model.eval()
+        lai_gt = self.compute_gt(batch)
+        reg_input = self.get_regression_input(batch)
+        lai_pred = unstand_lai(self.forward(reg_input))
+        return OutputLAI(lai_pred, reg_input, unstand_lai(lai_gt))
+
+    def compute_gt(self, batch: MMDCBatch, stand: bool = True) -> torch.Tensor:
+        """Compute LAI GT data wiht standardisation or not"""
+        gt = predict_variable_from_tensors(batch.s2_x, batch.s2_a, batch.s2_m,
+                                           self.model_snap)
+        if stand:
+            return stand_lai(gt)
+        return gt
+
+    def configure_optimizers(self) -> dict[str, Any]:
+        """A single optimizer with a LR scheduler"""
+        optimizer = torch.optim.Adam(params=self.model.parameters(),
+                                     lr=self.learning_rate)
+
+        training_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
+            optimizer, T_0=4, T_mult=2, eta_min=0, last_epoch=-1)
+        # scheduler = {
+        #     "scheduler": training_scheduler,
+        #     "interval": "epoch",
+        #     "monitor": "val/mse_loss",
+        #     "frequency": 1,
+        # }
+        return {
+            "optimizer": optimizer,
+            # "lr_scheduler": scheduler,
+        }
