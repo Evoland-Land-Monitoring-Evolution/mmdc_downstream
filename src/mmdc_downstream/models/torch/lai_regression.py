814e85814d8e3735765fbb46adad666d26c68534
Ekaterina Kalinicheva <ekaterina.kalinicheva@univ-tlse3.fr>
2024-01-08 15:03:07 +0000

    Linear layers instead of Conv2D with kernel=1

--- src/mmdc_downstream/models/torch/lai_regression.py
+++ src/mmdc_downstream/models/torch/lai_regression.py
@@ -30,6 +30,26 @@ class MMDCDownstreamRegressionModule(MMDCDownstreamBaseModule):
         """Forward pass"""
         return self.regression_model(data)
 
+    # def build_regression_model(self) -> nn.Sequential:
+    #     """Build regression model"""
+    #     input_size = self.config.reg_model.input_size
+    #     output_size = self.config.reg_model.output_size
+    #     hidden_sizes = self.config.reg_model.hidden_sizes
+    #     sizes = [input_size] + hidden_sizes
+    #     reg_layers = []
+    #     for i in range(len(sizes) - 1):
+    #         reg_layers.append((f'layer_{i}',
+    #                            nn.Conv2d(in_channels=sizes[i],
+    #                                      out_channels=sizes[i + 1],
+    #                                      kernel_size=1)))
+    #         reg_layers.append(('tanh', nn.Tanh()))
+    #     reg_layers.append((f'layer_{len(sizes)}',
+    #                        nn.Conv2d(in_channels=sizes[-1],
+    #                                  out_channels=output_size,
+    #                                  kernel_size=1)))
+    #
+    #     return nn.Sequential(OrderedDict(reg_layers)).to(self.device)
+
     def build_regression_model(self) -> nn.Sequential:
         """Build regression model"""
         input_size = self.config.reg_model.input_size
@@ -39,13 +59,11 @@ class MMDCDownstreamRegressionModule(MMDCDownstreamBaseModule):
         reg_layers = []
         for i in range(len(sizes) - 1):
             reg_layers.append((f'layer_{i}',
-                               nn.Conv2d(in_channels=sizes[i],
-                                         out_channels=sizes[i + 1],
-                                         kernel_size=1)))
+                               nn.Linear(in_features=sizes[i],
+                                         out_features=sizes[i + 1]).float()))
             reg_layers.append(('tanh', nn.Tanh()))
-        reg_layers.append((f'layer_{len(sizes)}',
-                           nn.Conv2d(in_channels=sizes[-1],
-                                     out_channels=output_size,
-                                     kernel_size=1)))
+        reg_layers.append((f'layer_{len(sizes)-1}',
+                           nn.Linear(in_features=sizes[-1],
+                                     out_features=output_size).float()))
 
         return nn.Sequential(OrderedDict(reg_layers)).to(self.device)
