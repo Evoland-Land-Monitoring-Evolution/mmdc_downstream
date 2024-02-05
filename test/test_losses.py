52165e4b5f128fb9640aa81e570cd6baf3aa248c
Ekaterina Kalinicheva <ekaterina.kalinicheva@univ-tlse3.fr>
2024-02-05 18:17:52 +0000

    PyTest

--- test/test_losses.py
+++ test/test_losses.py
@@ -3,6 +3,8 @@
 
 import torch
 
+from mmdc_downstream.models.components.losses import compute_losses
+from mmdc_downstream.models.components.metrics import compute_val_metrics
 from mmdc_downstream.models.lightning.lai_regression import MMDCBatch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -20,10 +22,42 @@ batch = MMDCBatch(
     dem_x=torch.rand((bs, 4, h, w)).to(device),
 )
 
+lai_gt = torch.zeros((bs, 1, h, w))
+lai_pred = torch.zeros((bs, 1, h, w))
+mask = torch.zeros((bs, 1, h, w))
+
+
+mask[0, 0, :2, :2] = 1
+lai_gt[0, 0, :2, :2] = torch.nan
+
 
 def test_losses():
     """Test losses"""
 
+    losses = compute_losses(
+        preds=lai_pred,
+        target=lai_gt,
+        mask=mask,
+        margin=0,
+        losses_list=["MSE", "RMSE", "L1"],
+    )
+
+    for name, value in losses.items():
+        assert value == 0
+
 
 def test_metrcis():
     """Test metrics"""
+    metrics = compute_val_metrics(
+        preds=lai_pred,
+        target=lai_gt,
+        mask=mask,
+        margin=0,
+        metrics_list=["RSE", "R2", "MAE", "MAPE"],
+    )
+
+    for name, value in metrics.items():
+        if name in ("RSE", "MAE", "MAPE"):
+            assert value == 0
+        else:
+            assert value == 1
