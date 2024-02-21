09b8df8c65307008280021ebd22b05e43331d73b
Ekaterina Kalinicheva <ekaterina.kalinicheva@univ-tlse3.fr>
2024-02-21 11:58:00 +0000

    Mask introduced

--- src/mmdc_downstream_pastis/models/components/losses.py
+++ src/mmdc_downstream_pastis/models/components/losses.py
@@ -1,29 +1,31 @@
 """ Custom losses for downstream task """
 
 import torch
+from einops import rearrange
 from torch.nn import CrossEntropyLoss
 
-from mmdc_singledate.models.components.losses import mask_and_flatten
 
+def compute_losses(
+    preds: torch.Tensor,
+    target: torch.Tensor,
+    mask: torch.Tensor | None,
+    losses_list: list[str] = ["cross_entropy"],
+) -> dict[str, torch.Tensor]:
+    """Computes classification losses"""
 
+    if mask is not None:
+        batch, classes, _, _ = preds.shape
 
-def compute_losses(preds: torch.Tensor,
-                   target: torch.Tensor,
-                   mask: torch.Tensor,
-                   losses_list: list[str] = ["cross"],
-                   margin: int = 0) -> dict[str, torch.Tensor]:
-    """Computes regression losses"""
-    H, W = preds.shape[-2:]
-
-    preds = mask_and_flatten(preds[:, margin:H - margin, margin:W - margin], mask)
-    target = mask_and_flatten(target[:, margin:H - margin, margin:W - margin], mask)
+        preds = rearrange(preds, "b c h w -> (b h w) c")
+        mask = rearrange(mask, "b h w -> (b h w)")
+        target = rearrange(target, "b h w -> (b h w)")
+        preds = preds[~mask]
+        target = target[~mask]
 
     losses_dict = {}
 
-
-    if "cross" in losses_list:
+    if "cross_entropy" in losses_list:
         cross = CrossEntropyLoss().to(preds.device)
         losses_dict["cross_entropy"] = cross(preds, target)
-
 
     return losses_dict
