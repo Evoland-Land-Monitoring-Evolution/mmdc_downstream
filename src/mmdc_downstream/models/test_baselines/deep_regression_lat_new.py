3c79964f888c0f8544bfb7b5c9199bcdedd988b3
Ekaterina Kalinicheva <ekaterina.kalinicheva@univ-tlse3.fr>
2024-04-02 13:53:47 +0000

    Optimization

--- src/mmdc_downstream/models/test_baselines/deep_regression_lat_new.py
+++ src/mmdc_downstream/models/test_baselines/deep_regression_lat_new.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from pathlib import Path
 
@@ -9,45 +10,12 @@ import torch.nn as nn
 import torch.optim as optim
 import tqdm
 from torch.utils.data import DataLoader, TensorDataset
+from torchmetrics.regression import MeanSquaredError
 
 from mmdc_downstream.snap.lai_snap import denormalize
-from mmdc_downstream.utils import get_logger
 
-log = get_logger(__name__)
-
-
-class MLP(nn.Module):
-    """Multilayer Perceptron for regression."""
-
-    def __init__(self, in_feat=11):
-        super().__init__()
-        self.layers = nn.Sequential(nn.Linear(in_feat, 5), nn.Tanh(), nn.Linear(5, 1))
-
-    def forward(self, x):
-        """Forward pass"""
-        return self.layers(x)
-
-
-lai_min = 0.000319182538301
-lai_max = 14.4675094548
-
-folder_data = "/work/scratch/data/kalinie/MMDC/jobs"
-
-path_train = os.path.join(
-    folder_data, "data_values_baseline_2024-02-27_14-47-18_train.csv"
-)
-path_val = os.path.join(folder_data, "data_values_baseline_2024-02-27_14-47-18_val.csv")
-path_test = os.path.join(
-    folder_data, "data_values_baseline_2024-02-27_14-47-18_test.csv"
-)
-
-column_names = pd.read_csv(path_test, index_col=0, nrows=0).columns.tolist()
-log.info("Columns to be used")
-log.info(column_names)
-
-df_train = pd.read_csv(path_train)
-df_test = pd.read_csv(path_test)
-df_val = pd.read_csv(path_val)
+logging.getLogger().setLevel(logging.INFO)
+np.random.seed(42)
 
 
 def rename_s1_cols(df: pd.DataFrame) -> pd.DataFrame:
@@ -65,19 +33,25 @@ def rename_s1_cols(df: pd.DataFrame) -> pd.DataFrame:
     )
 
 
-def get_value_label(path, selected_data, column_names):
+def get_value_label(path, selected_data, column_names, use_logvar):
     x_col = [
         i
         for i in column_names
         for data_type in selected_data
         if i.startswith(data_type)
     ]
-    df = pd.read_csv(path, usecols=x_col.extend("gt"))
+    if not use_logvar:
+        x_col = [i for i in x_col if "logvar" not in i]
+    use_cols = x_col + ["gt"]
+    logging.info("Columns to be used")
+    logging.info(use_cols)
+    df = pd.read_csv(path, usecols=use_cols)
     if len([col for col in x_col if col.startswith("s1")]) > 0:
+        logging.info([col for col in x_col if col.startswith("s1")])
         df = rename_s1_cols(df)
     y = df["gt"]  # centered
     x = df[x_col]
-    return x, y
+    return x, y, x_col
 
 
 def select_s1(x_train, x_test, x_val, y_train, y_test, y_val, orbit):
@@ -94,33 +68,73 @@ def select_s1(x_train, x_test, x_val, y_train, y_test, y_val, orbit):
     )
 
 
+class MLP(nn.Module):
+    """Multilayer Perceptron for regression."""
+
+    def __init__(self, in_feat=11):
+        super().__init__()
+        self.layers = nn.Sequential(nn.Linear(in_feat, 5), nn.Tanh(), nn.Linear(5, 1))
+
+    def forward(self, x):
+        """Forward pass"""
+        return self.layers(x)
+
+
+logging.info("Starting everything")
+
+lai_min = torch.tensor(0.000319182538301)
+lai_max = torch.tensor(14.4675094548)
+
+folder_data = "/work/scratch/data/kalinie/MMDC/jobs"
+
+path_train = os.path.join(
+    folder_data, "data_values_baseline_2024-02-27_14-47-18_train.csv"
+)
+path_val = os.path.join(folder_data, "data_values_baseline_2024-02-27_14-47-18_val.csv")
+path_test = os.path.join(
+    folder_data, "data_values_baseline_2024-02-27_14-47-18_test.csv"
+)
+
+column_names = pd.read_csv(path_test, index_col=0, nrows=0).columns.tolist()
+logging.info("All columns")
+logging.info(column_names)
+
 selected_data = ["exp_lat", "s1_lat", "s2_lat", "s1_asc", "s1_desc", "s1_mask"]
 # train_data = \
 #     ["s2_input", "exp_lat", "s1_lat", "s2_lat", 's1_asc', "s1_desc", "s1_mask"]
 
 selected_data = ["s2_lat"]
 
-log.info("Opening train df")
-x_train_all, y_train = get_value_label(path_train, selected_data, column_names)
-log.info("Opening val df")
-x_val_all, y_val = get_value_label(path_val, selected_data, column_names)
-log.info("Opening test df")
-x_test_all, y_test = get_value_label(path_test, selected_data, column_names)
+USE_LOGVAR = True
+
+logging.info("Opening train df")
+# 102805129
+x_train_all, y_train, x_col = get_value_label(
+    path_train, selected_data, column_names, USE_LOGVAR
+)
+logging.info("Opening val df")
+# 25720720
+x_val_all, y_val, _ = get_value_label(path_val, selected_data, column_names, USE_LOGVAR)
+logging.info("Opening test df")
+# 12900234
+x_test_all, y_test, _ = get_value_label(
+    path_test, selected_data, column_names, USE_LOGVAR
+)
 
 results = {}
 y_test_denorm = {}
 
 for data_type in selected_data:
     if data_type != "s1_mask":
-        train_col = [i for i in df_train.columns if i.startswith(data_type)]
-        log.info(train_col)
+        train_col = [i for i in x_col if i.startswith(data_type)]
+        logging.info(train_col)
         if train_col[0].startswith("s1_asc"):
             x_train_all, x_test_all, x_val_all, y_train, y_test, y_val = select_s1(
                 x_train_all, x_test_all, x_val_all, y_train, y_test, y_val, orbit="asc"
             )
 
         if train_col[0].startswith("s1_desc"):
-            x_train, x_test, x_val, y_train, y_test, y_val = select_s1(
+            x_train_all, x_test_all, x_val_all, y_train, y_test, y_val = select_s1(
                 x_train_all, x_test_all, x_val_all, y_train, y_test, y_val, orbit="desc"
             )
 
@@ -136,26 +150,39 @@ for data_type in selected_data:
         y_test = torch.tensor(y_test.values, dtype=torch.float32).reshape(-1, 1)
 
         model = MLP(in_feat=X_train.shape[1])
-        train_dataset = TensorDataset(X_train, y_train)
-        val_dataset = TensorDataset(X_val, y_val)
-        test_dataset = TensorDataset(X_test, y_test)
-
+        print(
+            denormalize(y_train, lai_min, lai_max).min(),
+            denormalize(y_train, lai_min, lai_max).max(),
+        )
         train_dl = DataLoader(
-            train_dataset, batch_size=50 * 64 * 64, shuffle=True, num_workers=2
+            TensorDataset(X_train, y_train),
+            batch_size=200 * 64 * 64,
+            shuffle=True,
+            num_workers=4,
+        )
+        print(
+            denormalize(y_val, lai_min, lai_max).min(),
+            denormalize(y_train, lai_min, lai_max).max(),
         )
         val_dl = DataLoader(
-            val_dataset, batch_size=200 * 64 * 64, shuffle=False, num_workers=2
+            TensorDataset(X_val, y_val),
+            batch_size=400 * 64 * 64,
+            shuffle=False,
+            num_workers=4,
         )
         test_dl = DataLoader(
-            test_dataset, batch_size=200 * 64 * 64, shuffle=False, num_workers=2
+            TensorDataset(X_test, y_test),
+            batch_size=400 * 64 * 64,
+            shuffle=False,
+            num_workers=4,
         )
 
         # loss function and optimizer
-        loss_fn = nn.MSELoss()  # mean square error
-        optimizer = optim.Adam(model.parameters(), lr=0.001)
+        loss_fn = MeanSquaredError(squared=False)  # root mean square error
+        optimizer = optim.Adam(model.parameters(), lr=0.00001)
 
         # training parameters
-        n_epochs = 100  # number of epochs to run
+        n_epochs = 50  # number of epochs to run
 
         # Hold the best model
         best_mse = np.inf  # init to infinity
@@ -168,17 +195,19 @@ for data_type in selected_data:
         folder = (
             f"bs_{train_dl.batch_size}_lr_{optimizer.param_groups[0]['lr']}_hidden_5"
         )
+        if USE_LOGVAR:
+            folder += "_logvar"
         output_folder = os.path.join(path, folder)
-        log.info(output_folder)
+        logging.info(output_folder)
         Path(output_folder).mkdir(exist_ok=True, parents=True)
 
         # Run the training loop
         for epoch in range(n_epochs):  # 5 epochs at maximum
             if epoch == 5:
-                optimizer.param_groups[0]["lr"] = 0.0001
+                optimizer.param_groups[0]["lr"] = 0.00001
 
             # Print epoch
-            log.info(f"Starting epoch {epoch + 1}")
+            logging.info(f"Starting epoch {epoch + 1}")
 
             # Set current loss value
             current_loss_train = 0.0
@@ -209,11 +238,11 @@ for data_type in selected_data:
 
                 # Print statistics
                 current_loss_train += loss.item()
-            log.info(
+            logging.info(
                 "Train Loss after epoch %5d: %.3f"
                 % (
                     epoch + 1,
-                    current_loss_train / int(len(train_dataset) / train_dl.batch_size),
+                    current_loss_train / train_dl.__len__(),
                 )
             )
 
@@ -237,31 +266,35 @@ for data_type in selected_data:
 
                 # Print statistics
                 current_loss_val += loss.item()
-            log.info(
+            logging.info(
                 "Validation Loss after epoch %5d: %.3f"
                 % (
                     epoch + 1,
-                    current_loss_val / int(len(val_dataset) / val_dl.batch_size),
+                    current_loss_val / val_dl.__len__(),
                 )
             )
             preds = np.concatenate(preds, 0)
 
-            pred = denormalize(preds, lai_min, lai_max)
-            y_val_denorm = (
-                denormalize(y_val, lai_min, lai_max).flatten().to(torch.float32)
-            )
+            idx = np.random.choice(len(preds), int(len(preds) * 0.05), replace=False)
+            preds = denormalize(torch.Tensor(preds), lai_min, lai_max)
+            y_val_denorm = denormalize(y_val, lai_min, lai_max).flatten()
+            print(y_val_denorm)
 
-            log.info("max", pred.max())
-            log.info("min", pred.min())
+            logging.info("pred max " + str(preds.max()))
+            logging.info("pred min " + str(preds.min()))
 
             plt.close()
-            plt.scatter(y_val_denorm, pred, s=0.1)
-            plt.plot(y_val_denorm, y_val_denorm)
-            plt.xlabel(
+            plt.scatter(preds[idx], y_val_denorm[idx], s=0.1)
+            plt.plot(y_val_denorm[idx], y_val_denorm[idx])
+
+            plt.title(
                 "loss="
-                + str(np.round(loss_fn(torch.Tensor(pred), y_val_denorm).item(), 4))
+                + str(np.round(loss_fn(torch.Tensor(preds), y_val_denorm).item(), 4))
             )
+            plt.xlabel("pred")
+            plt.ylabel("gt")
+
             plt.savefig(output_folder + f"/mlp_{epoch}.png")
 
             # Process is complete.
-        log.info("Training process has finished.")
+        logging.info("Training process has finished.")
