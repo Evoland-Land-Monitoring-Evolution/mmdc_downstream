dc3016aba54ac45b22af1d243ff6814ec4bbf303
Ekaterina <ek.kalinicheva@gmail.com>
2024-05-24 15:01:48 +0200

    FEAT: Prepare tiles for TCD

---
+++ src/bin/prepare_tcd_tile.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+# copyright: (c) 2024 cesbio / centre national d'Etudes Spatiales
+"""
+This module computes different bio-physical variables for MMDC dataset
+"""
+
+import argparse
+import logging
+import os
+from pathlib import Path
+
+from mmdc_downstream_tcd.tile_processing.prepare_tile import prepare_tiles
+
+my_logger = logging.getLogger(__name__)
+
+
+def get_parser() -> argparse.ArgumentParser:
+    """
+    Generate argument parser for CLI
+    """
+    arg_parser = argparse.ArgumentParser(
+        os.path.basename(__file__),
+        description="Serialize as torch tensors the data generated for the"
+        " MMDC-DataCollection project",
+    )
+
+    arg_parser.add_argument(
+        "--loglevel",
+        default="INFO",
+        choices=("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"),
+        help="Logger level (default: INFO. Should be one of "
+        "(DEBUG, INFO, WARNING, ERROR, CRITICAL)",
+    )
+
+    arg_parser.add_argument(
+        "--folder_data",
+        type=str,
+        help="input folder",
+        # default="/work/CESBIO/projects/DeepChange/Ekaterina/TCD/OpenEO_data/t32tnt/",
+        default=f"{os.environ['SCRATCH']}/scratch_data/TCD/OpenEO_data/t32tnt/"
+        # required=True
+    )
+
+    arg_parser.add_argument(
+        "--output_path",
+        type=str,
+        help="output folder",
+        # default="/work/scratch/data/kalinie/TCD/OpenEO_data/t32tnt/encoded"
+        default=f"{os.environ['SCRATCH']}/results/TCD/t32tnt"
+        # required=True
+    )
+
+    arg_parser.add_argument(
+        "--months_folders",
+        type=str,
+        help="list of available folders with months",
+        default=["3", "4", "5", "6", "7", "8", "9", "10"]
+        # required=False,
+    )
+
+    arg_parser.add_argument(
+        "--window",
+        type=int,
+        help="size of sliding window",
+        default=768
+        # required=False,
+    )
+
+    arg_parser.add_argument(
+        "--margin",
+        type=int,
+        help="size of margin",
+        default=40
+        # required=False,
+    )
+
+    arg_parser.add_argument(
+        "--satellites",
+        type=int,
+        help="satellites to prepare",
+        default=["s2", "s1_asc", "s1_desc"]
+        # required=False,
+    )
+
+    return arg_parser
+
+
+if __name__ == "__main__":
+    # Parser arguments
+    parser = get_parser()
+    args = parser.parse_args()
+
+    # Configure logging
+    numeric_level = getattr(logging, args.loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError(f"Invalid log level: {args.loglevel}")
+
+    logging.basicConfig(
+        level=numeric_level,
+        datefmt="%y-%m-%d %H:%M:%S",
+        format="%(asctime)s :: %(levelname)s :: %(message)s",
+    )
+
+    output_path = os.path.join(
+        args.output_path, f"prepared_tiles_w_{args.window}_m_{args.margin}"
+    )
+
+    for sat in args.satellites:
+        Path(os.path.join(output_path, sat)).mkdir(parents=True, exist_ok=True)
+
+    prepare_tiles(
+        args.folder_data,
+        args.months_folders,
+        args.window,
+        output_path,
+        args.margin,
+        args.satellites,
+    )
---
+++ src/mmdc_downstream_tcd/tile_processing/prepare_tile.py
@@ -0,0 +1,81 @@
+import logging
+import os
+import warnings
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import torch
+
+from mmdc_downstream_tcd.tile_processing.utils import (
+    generate_coord_matrix,
+    get_slices,
+    prepare_patch,
+)
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+log = logging.getLogger(__name__)
+
+
+def prepare_tiles(
+    folder_data: str | Path,
+    months_folders: list[str | int],
+    window: int,
+    output_folder: str | Path,
+    margin: int = 50,
+    satellites: list[str] = ("s2", "s1_asc", "s1_desc"),
+):
+    """
+    Encode a tile with MMDC algorithm with a sliding window
+    """
+
+    slices_list = get_slices(
+        folder_data, months_folders, window, margin=margin, drop_incomplete=True
+    )
+
+    dates_meteo: dict[str, pd.DataFrame] = {}
+    s2_dates = np.load(os.path.join(folder_data, "dates_s2.npy"))
+    s1_dates_asc = np.load(os.path.join(folder_data, "dates_s1_asc_good.npy"))
+    s1_dates_desc = np.load(os.path.join(folder_data, "dates_s1_desc_good.npy"))
+
+    # for enum, sliced in enumerate(slices_list[::-1]):
+    #     enum = len(slices_list) - enum - 1
+    for enum, sliced in enumerate(slices_list):
+        if not np.all(
+            [
+                Path(os.path.join(output_folder, sat, f"{sat}_tile_{enum}.pt")).exists()
+                for sat in satellites
+            ]
+        ):
+            xy_matrix = generate_coord_matrix(sliced)
+
+            data, dates_meteo = prepare_patch(
+                folder_data,
+                months_folders,
+                sliced,
+                s2_dates,
+                s1_dates_asc,
+                s1_dates_desc,
+                dates_meteo,
+                satellites,
+            )
+            data["matrix"] = {
+                "x_min": xy_matrix[0, 0].min(),
+                "x_max": xy_matrix[0, 0].max(),
+                "y_min": xy_matrix[1, :, 0].min(),
+                "y_max": xy_matrix[1, :, 0].max(),
+            }
+
+            for sat in satellites:
+                torch.save(
+                    {"data": data[sat], "matrix": data["matrix"]},
+                    os.path.join(output_folder, sat, f"{sat}_tile_{enum}.pt"),
+                )
+                if not Path(
+                    os.path.join(output_folder, sat, f"{sat}_dates.pt")
+                ).exists():
+                    torch.save(
+                        dates_meteo[sat],
+                        os.path.join(output_folder, sat, f"{sat}_dates.pt"),
+                    )
