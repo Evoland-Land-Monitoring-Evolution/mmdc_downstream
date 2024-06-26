45701d1a2ae29aae147ef37af7a2cc9926fc00b7
Ekaterina <ek.kalinicheva@gmail.com>
2024-06-26 15:52:13 +0200

    Small changes

--- src/mmdc_downstream_tcd/tile_processing/encode_tile_alise.py
+++ src/mmdc_downstream_tcd/tile_processing/encode_tile_alise.py
@@ -32,7 +32,9 @@ def encode_tile_alise(
     satellites: list[str] = ["s2"],
 ):
     """
-    Encode a tile with MMDC algorithm with a sliding window
+    Encode a tile with ALISE algorithm with a sliding window
+    We divide the sliding window of 768 pixels into 4 overlapping patches
+    to overcome memory problems
     """
     sess_opt = onnxruntime.SessionOptions()
     sess_opt.intra_op_num_threads = 16
@@ -73,14 +75,13 @@ def encode_tile_alise(
         for f in os.listdir(os.path.join(folder_prepared_data, satellites[0]))
         if f.startswith(f"{satellites[0]}_tile_") and f.endswith(".pt")
     ]
-    # for enum in range(len(available_tiles))[::-1]:
 
     for enum in range(len(available_tiles)):
         if not np.all(
             [
                 Path(
                     os.path.join(
-                        output_folder, f"gt_tcd_t32tnt_encoded_alise_{sat}_{enum}.csv"
+                        output_folder, sat, f"gt_tcd_t32tnt_encoded_{sat}_{enum}.csv"
                     )
                 ).exists()
                 for sat in satellites
@@ -128,53 +129,35 @@ def encode_tile_alise(
             # Prepare patch
             s2_ref_norm = rearrange(
                 transform(rearrange(s2_ref, "t c h w -> c t h w")),
-                "c t h w -> 1 t c h w",
+                "c t h w -> 1 t h w c",
             )
-            small_patch_size = 64
 
-            x2 = small_patch_size
-            y2 = small_patch_size
-            for x1 in range(0, window, int(small_patch_size / 2)):
-                for y1 in range(0, window, int(small_patch_size / 2)):
-                    log.info(f"Patch {x1}:{x2}, {y1}:{y2}")
-                    sits = s2_ref_norm.numpy()[:, :, :, x1:x2, y1:y2]
-                    if sits.shape[-2] == sits.shape[-1] == 32:
-                        new_sits = np.zeros((*sits.shape[:3], 64, 64))
-                        new_sits[:, :, :, :32, :32] = sits
-                        new_sits[:, :, :, 32:, :32] = np.flip(sits, -1)
-
-                        new_sits[:, :, :, :32, 32:] = np.flip(sits, -2)
-
-                        new_sits[:, :, :, 32:, 32:] = np.flip(np.flip(sits, -2), -1)
-
-                        sits = new_sits.astype(np.float32)
-                    elif sits.shape[-1] == 32:
-                        new_sits = np.zeros((*sits.shape[:3], 64, 64))
-                        new_sits[:, :, :, :, :32] = sits
-                        new_sits[:, :, :, :, 32:] = np.flip(sits, -2)
-                        sits = new_sits.astype(np.float32)
-                    elif sits.shape[-2] == 32:
-                        new_sits = np.zeros((*sits.shape[:3], 64, 64))
-                        new_sits[:, :, :, :32, :] = sits
-                        new_sits[:, :, :, 32:, :] = np.flip(sits, -1)
-                        sits = new_sits.astype(np.float32)
-
+            log.info(s2_ref_norm.shape)
+            margin_local = 32
+            for x in range(0, window, 352):
+                if x + 416 > window:
+                    continue
+                for y in range(0, window, 352):
+                    if y + 416 > window:
+                        continue
+                    log.info(f"Patch {x}:{x+416}, {y}:{y+416}")
+                    sits = s2_ref_norm.numpy()[:, :, y : y + 416, x : x + 416, :]
+                    log.info(sits.shape)
                     input = {
                         "sits": sits,
                         "tpe": doy[None, :].numpy(),
-                        "padd_mask": torch.zeros(1, s2_ref_norm.shape[1])
-                        .bool()
-                        .numpy(),
+                        "padd_mask": torch.zeros(1, sits.shape[1]).bool().numpy(),
                     }
+
                     ort_out = ort_session.run(None, input)[0]  # (1, 10, 64, 64, 64)
-
                     encoded_patch["s2_lat_mu"][
-                        :, :, x1 + 16 : x1 + 16 + 32, y1 + 16 : y1 + 16 + 32
-                    ] = ort_out[0, :, :, 16:-16, 16:-16]
-
-                    y2 += int(small_patch_size / 2)
-                y2 = small_patch_size
-                x2 += int(small_patch_size / 2)
+                        :,
+                        :,
+                        y + margin_local : y + 416 - margin_local,
+                        x + margin_local : x + 416 - margin_local,
+                    ] = ort_out[
+                        0, :, :, margin_local:-margin_local, margin_local:-margin_local
+                    ]
 
             encoded_patch["matrix"] = xy_matrix
             encoded_patch["s2_lat_mu"] = encoded_patch["s2_lat_mu"][
@@ -183,6 +166,7 @@ def encode_tile_alise(
 
             encoded_patch["s2_doy"] = np.arange(encoded_patch["s2_lat_mu"].shape[0])
 
+            # Save encoded patch
             torch.save(
                 {
                     "img": encoded_patch["s2_lat_mu"].astype(np.float32),
@@ -193,9 +177,10 @@ def encode_tile_alise(
                         "y_max": xy_matrix[1, :, 0].max(),
                     },
                 },
-                os.path.join(output_folder, f"s2_{enum}.pt"),
+                os.path.join(output_folder, sat, f"s2_{enum}.pt"),
             )
 
+            # Save GT data
             if ind.size > 0 and tcd_values_sliced is not None:
                 for sat in satellites:
                     df_gt, days = extract_gt_points(
@@ -208,20 +193,20 @@ def encode_tile_alise(
                     df_gt.to_csv(
                         os.path.join(
                             output_folder,
+                            sat,
                             f"gt_tcd_t32tnt_encoded_{sat}_{enum}.csv",
                         )
                     )
                     if not Path(
-                        os.path.join(output_folder, f"gt_tcd_t32tnt_days_{sat}.npy")
+                        os.path.join(
+                            output_folder, sat, f"gt_tcd_t32tnt_days_{sat}.npy"
+                        )
                     ).exists():
                         np.save(
                             os.path.join(
-                                output_folder, f"gt_tcd_t32tnt_days_{sat}.npy"
+                                output_folder, sat, f"gt_tcd_t32tnt_days_{sat}.npy"
                             ),
                             days,
                         )
-
-                # torch.save(encoded_patch,
-                #            os.path.join(output_folder, f"Encoded_patch_{enum}.pt"))
 
             del encoded_patch
