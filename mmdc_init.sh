6c8a3170bf0c22f968613f3f73a5901c230ff29f
Ekaterina <ek.kalinicheva@gmail.com>
2024-08-06 10:52:09 +0200

    MMDC_init

--- mmdc_init.sh
+++ mmdc_init.sh
@@ -1,4 +1,4 @@
 module purge
-module load anaconda-py3
+module load conda
 #conda activate /work/scratch/env/${USER}/virtualenv/mmdc-sgld-py3109-torch-200-light200
 conda activate mmdc_safe_environment
