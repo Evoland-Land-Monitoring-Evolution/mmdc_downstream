bcc8964eff29f1dca3882eebab8614b96107a74f
Ekaterina Kalinicheva <ekaterina.kalinicheva@univ-tlse3.fr>
2025-03-06 14:38:41 +0000

    REFAC: New executable files

---
+++ src/bin/pastis_classif_train.py
@@ -0,0 +1,25 @@
+import dotenv
+import hydra
+from omegaconf import DictConfig
+
+# load environment variables from `.env` file if it exists
+# recursively searches for `.env` in all folders starting from work dir
+dotenv.load_dotenv(override=True)
+
+
+@hydra.main(config_path="../../configs/", config_name="train.yaml", version_base=None)
+def main(config: DictConfig):
+    # Imports can be nested inside @hydra.main to optimize tab completion
+    # https://github.com/facebookresearch/hydra/issues/934
+    from mmdc_downstream_pastis.training_pipeline import train
+    from mmdc_downstream_pastis.utils import extras
+
+    #
+    # # Applies optional utilities
+    extras(config)
+    # Train model
+    return train(config)
+
+
+if __name__ == "__main__":
+    main()
